9beddf32cba5fa88372dad51065101480cf72873
HoloGrave <172952189+HoloGrave@users.noreply.github.com>
2025-10-22 10:43:38 -0400

    Class Creation

--- src/Adventurer.java
+++ src/Adventurer.java
@@ -1,2 +1,73 @@
-public class Adventurer {
+import java.util.List;
+
+public class Adventurer implements Comparable{
+    String name;
+    int age;
+    String role;
+    double goldEarned;
+    List<Skill> skills;
+
+    public Adventurer(int age, String name, String role, List<Skill> skills, double goldEarned) {
+        this.age = age;
+        this.name = name;
+        this.role = role;
+        this.skills = skills;
+        this.goldEarned = goldEarned;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public double getGoldEarned() {
+        return goldEarned;
+    }
+
+    public void setGoldEarned(double goldEarned) {
+        this.goldEarned = goldEarned;
+    }
+
+    public List<Skill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<Skill> skills) {
+        this.skills = skills;
+    }
+
+    @Override
+    public String toString() {
+        return "Adventurer{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", role='" + role + '\'' +
+                ", goldEarned=" + goldEarned +
+                ", skills=" + skills +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
 }
--- src/Guild.java
+++ src/Guild.java
@@ -1,2 +1,40 @@
-public class Guild {
+import java.util.List;
+
+public class Guild implements Comparable{
+    String name;
+    List<Adventurer> adventurers;
+
+    public Guild(String name, List<Adventurer> adventurers) {
+        this.name = name;
+        this.adventurers = adventurers;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Adventurer> getAdventurers() {
+        return adventurers;
+    }
+
+    public void setAdventurers(List<Adventurer> adventurers) {
+        this.adventurers = adventurers;
+    }
+
+    @Override
+    public String toString() {
+        return "Guild{" +
+                "name='" + name + '\'' +
+                ", adventurers=" + adventurers +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
 }
